0cbb1c34189720386dabc251d23285cf89a6d9b9
Mason Wright <masonwright14@gmail.com>
2018-02-19 14:42:48 -0500

    adding random graph generation code

--- src/main/MainGraphGen02.java
+++ src/main/MainGraphGen02.java
@@ -24,7 +24,60 @@ public final class MainGraphGen02 {
 			throw new IllegalStateException(
 				"Need 1 argument: graphFolder");
 		}
-		String folderPath = args[0];
+		// makeSepLayerGraph29(args[0]);
+		makeRandomGraph30(args[0]);
+	}
+	
+	public static void makeRandomGraph30(final String folderPath) {
+		final int numNode = 30;
+		final int numEdge = 100;
+		final int numTarget = 6;
+		final double nodeActTypeRatio = 0.0;
+		final double aRewardLB = 5.0;
+		final double aRewardUB = 10.0;
+		final double dPenaltyLB = -10.0;
+		final double dPenaltyUB = -5.0;
+		
+		final double aNodeCostLB = -1.0;
+		final double aNodeCostUB = -0.5;
+		final double aEdgeCostLB = -1.0;
+		final double aEdgeCostUB = -0.5;
+		final double dCostLB = -2.0;
+		final double dCostUB = -1.0;
+
+		final double aNodeActProbLB = 0.8;
+		final double aNodeActProbUB = 1.0;
+		final double aEdgeActProbLB = 0.6;
+		final double aEdgeActProbUB = 0.8;
+		final double minPosActiveProb = 0.8;
+		final double maxPosActiveProb = 1.0;
+		final double minPosInactiveProb = 0.0;
+		final double maxPosInactiveProb = 0.2;
+		
+		Node.resetCounter();
+		Edge.resetCounter();
+		RandomDataGenerator rnd = new RandomDataGenerator();
+		rnd.reSeed(System.currentTimeMillis());
+		
+		final int numSample = 2;
+		
+		genRandomGraph(
+			folderPath, numNode,
+			numEdge, numTarget,
+			nodeActTypeRatio, aRewardLB,
+			aRewardUB, dPenaltyLB,
+			dPenaltyUB, aNodeCostLB,
+			aNodeCostUB, aEdgeCostLB,
+			aEdgeCostUB, dCostLB,
+			dCostUB, aNodeActProbLB,
+			aNodeActProbUB, aEdgeActProbLB,
+			aEdgeActProbUB, minPosActiveProb,
+			maxPosActiveProb, minPosInactiveProb,
+			maxPosInactiveProb, rnd,
+			numSample);
+	}
+	
+	public static void makeSepLayerGraph29(final String folderPath) {
 		final int numTarget = 7;
 		final double nodeActTypeRatio = 0.0;
 		final double aRewardLB = 10.0;
