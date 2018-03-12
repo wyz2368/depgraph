39c398ff70e208d3fc188a9e22b0d89b0bbde070
Mason Wright <masonwright14@gmail.com>
2018-03-11 22:08:13 -0400

    fixes

--- pythoncode/depgraph_java_no_net.py
+++ pythoncode/depgraph_java_no_net.py
@@ -9,38 +9,43 @@ from py4j.java_gateway import JavaGateway
 JAVA_GAME = None
 GATEWAY = None
 
-def init():
+class DepgraphJavaEnvAttNoNet():
     '''
-    Set up the connection to the Java game.
+    Runs the Java game environment.
     '''
-    # https://www.py4j.org/getting_started.html
-    global GATEWAY
-    GATEWAY = JavaGateway()
-    global JAVA_GAME
-    JAVA_GAME = GATEWAY.entry_point.getGame()
+    def init(self):
+        '''
+        Set up the connection to the Java game.
+        '''
+        # https://www.py4j.org/getting_started.html
+        global GATEWAY
+        GATEWAY = JavaGateway()
+        global JAVA_GAME
+        JAVA_GAME = GATEWAY.entry_point.getGame()
 
-def reset_and_run_once():
-    '''
-    Reset the game and run until it ends.
-    '''
-    JAVA_GAME.resetAndRunOnce()
+    def reset_and_run_once(self):
+        '''
+        Reset the game and run until it ends.
+        '''
+        JAVA_GAME.resetAndRunOnce()
 
-def get_attacker_reward():
-    '''
-    Get the total discounted reward of the opponent (attacker) in the current game.
-    '''
-    return JAVA_GAME.getOpponentTotalPayoff()
+    def get_attacker_reward(self):
+        '''
+        Get the total discounted reward of the opponent (attacker) in the current game.
+        '''
+        return JAVA_GAME.getOpponentTotalPayoff()
 
-def get_defender_reward():
-    '''
-    Get the total discounted reward of self (defender) in the current game.
-    '''
-    return JAVA_GAME.getSelfTotalPayoff()
+    def get_defender_reward(self):
+        '''
+        Get the total discounted reward of self (defender) in the current game.
+        '''
+        return JAVA_GAME.getSelfTotalPayoff()
 
-def close_gateway():
-    '''
-    Close the connection to the Java program.
-    '''
-    GATEWAY.close()
-    GATEWAY.close_callback_server()
-    GATEWAY.shutdown()
+    def close_gateway(self):
+        '''
+        Close the connection to the Java program.
+        '''
+        GATEWAY.close()
+        GATEWAY.close_callback_server()
+        GATEWAY.shutdown()
+    
