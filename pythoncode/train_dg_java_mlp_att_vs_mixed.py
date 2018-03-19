cb2a77a1e7b159f06ff3a4ce3f51e8cb55ea0fae
Mason Wright <masonwright14@gmail.com>
2018-03-19 16:53:47 -0400

    adding code to train att against mixed net and heuristic def

---
+++ pythoncode/train_dg_java_mlp_att_vs_mixed.py
@@ -0,0 +1,45 @@
+'''
+Trains a multilayer perceptron to play the depgraph game against
+a defender that can mix over heuristic and network strategies.
+'''
+import time
+import gym
+
+from baselines import deepq
+
+def main():
+    '''
+    Makes the depgraph environment, builds a multilayer perceptron model,
+    trains the model, and saves the result.
+    '''
+    env_name = "DepgraphJavaEnvVsMixedDef-v0"
+    print("Environment: " + env_name)
+
+    start = time.time()
+    env = gym.make(env_name)
+    model = deepq.models.mlp([256, 256])
+    act = deepq.learn_multiple_nets(
+        env,
+        q_func=model,
+        lr=5e-5,
+        max_timesteps=1000000,
+        buffer_size=30000,
+        exploration_fraction=0.5,
+        exploration_final_eps=0.03,
+        checkpoint_freq=30000,
+        print_freq=250,
+        param_noise=False,
+        gamma=0.99,
+        ep_mean_length=250
+    )
+    model_name = "dg_dqmlp_rand30NoAnd_B_epoch2_att.pkl"
+    print("Saving model to: " + model_name)
+    act.save(model_name)
+    end = time.time()
+    elapsed = end - start
+    minutes = elapsed // 60
+    print("Minutes taken: " + str(minutes))
+    print("Opponent was: " + env_name)
+
+if __name__ == '__main__':
+    main()
