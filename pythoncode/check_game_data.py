53a8974df04dd24689adb7d43ca2038ef3ac9fee
Mason Wright <masonwright14@gmail.com>
2018-05-04 12:17:27 -0400

    adding more checks to game file

--- pythoncode/check_game_data.py
+++ pythoncode/check_game_data.py
@@ -19,7 +19,91 @@ def print_json(file_name, json_obj):
     with open(file_name, 'w') as my_file:
         json.dump(json_obj, my_file)
 
-def check_game_data(game_data):
+def get_defender_strats(game_data):
+    '''
+    Returns the list of strategies for the defender role.
+    '''
+    for role in game_data["roles"]:
+        if role["name"] == "defender":
+            return role["strategies"]
+    raise ValueError("Defender not found: " + str(game_data["roles"]))
+
+def get_attacker_strats(game_data):
+    '''
+    Returns the list of strategies for the attacker role.
+    '''
+    for role in game_data["roles"]:
+        if role["name"] == "attacker":
+            return role["strategies"]
+    raise ValueError("Attacker not found: " + str(game_data["roles"]))
+
+def are_payoffs_found(game_data, defender, attacker):
+    '''
+    Returns True iff there is a payoff entry for the defender-attacker pair.
+    '''
+    for profile in game_data["profiles"]:
+        matches_def = False
+        matches_att = False
+        for group in profile["symmetry_groups"]:
+            if group["strategy"] == defender and group["role"] == "defender":
+                matches_def = True
+            if group["strategy"] == attacker and group["role"] == "attacker":
+                matches_att = True
+        if matches_def and matches_att:
+            return True
+    return False
+
+def check_for_missing_payoffs(game_data):
+    '''
+    Check for combinations of attacker strategy and defender strategy from the role strategy
+    lists, that do not have corresponding payoffs.
+    '''
+    defender_strats = get_defender_strats(game_data)
+    attacker_strats = get_attacker_strats(game_data)
+    found_missing = False
+    for defender in defender_strats:
+        for attacker in attacker_strats:
+            if not are_payoffs_found(game_data, defender, attacker):
+                print("Missing payoffs: " + defender + " " + attacker)
+                found_missing = True
+    if not found_missing:
+        print("No missing payoffs.")
+
+def check_for_extra_payoffs(game_data):
+    '''
+    Check for payoff entries where either the defender strategy or attacker strategy
+    is not present in the role strategy list.
+    '''
+    defender_strats = get_defender_strats(game_data)
+    attacker_strats = get_attacker_strats(game_data)
+
+    missing_defs = set()
+    missing_atts = set()
+    for profile in game_data["profiles"]:
+        defender = None
+        attacker = None
+        for group in profile["symmetry_groups"]:
+            if group["role"] == "defender":
+                defender = group["strategy"]
+            if group["role"] == "attacker":
+                attacker = group["strategy"]
+        if defender is None or attacker is None:
+            raise ValueError("Invalid profile: " + str(profile))
+        if defender not in defender_strats or attacker not in attacker_strats:
+            print("Extra payoffs: " + defender + " " + attacker)
+        if defender not in defender_strats:
+            missing_defs.add(defender)
+        if attacker not in attacker_strats:
+            missing_atts.add(attacker)
+    if missing_defs:
+        print("Missing defenders: " + str(missing_defs))
+    if missing_atts:
+        print("Missing attackers: " + str(missing_atts))
+    if not missing_atts and not missing_defs:
+        print("No extra payoffs found.")
+
+
+def check_for_duplicates(game_data):
     '''
     -- check for duplicate profiles, and print any out with their values
     '''
@@ -65,7 +149,7 @@ def check_game_data(game_data):
                         print(str(def_payoff) + "\t" + str(att_payoff))
                 print("\n")
     if not any_problems:
-        print("Checks out OK.")
+        print("No duplicate payoffs found.")
 
 # python check_game_data.py game_3014_5.json
 def main(game_file):
@@ -75,7 +159,9 @@ def main(game_file):
     and print out their names and all payoff tuples
     '''
     game_data = get_json_data(game_file)
-    check_game_data(game_data)
+    check_for_duplicates(game_data)
+    check_for_missing_payoffs(game_data)
+    check_for_extra_payoffs(game_data)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
