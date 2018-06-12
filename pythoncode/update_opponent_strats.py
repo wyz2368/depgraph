b67c4ee44d473cbf69fa3ee6844370c9e7c75ab7
Mason Wright <masonwright14@gmail.com>
2018-06-12 10:24:38 -0400

    bug fixes

--- pythoncode/update_opponent_strats.py
+++ pythoncode/update_opponent_strats.py
@@ -12,8 +12,8 @@ def main(config_env_short_name, tsv_env_short_name, new_epoch):
     tsv_name_att = tsv_env_short_name + "_epoch" + str(new_epoch) + "_att.tsv"
     tsv_name_def = tsv_env_short_name + "_epoch" + str(new_epoch) + "_def.tsv"
 
-    write_line(config_file_name_att, tsv_name_att)
-    write_line(config_file_name_def, tsv_name_def)
+    write_line(config_file_name_att, "ATT_MIXED_STRAT_FILE = \"" + tsv_name_att + "\"")
+    write_line(config_file_name_def, "DEF_MIXED_STRAT_FILE = \"" + tsv_name_def + "\"")
 
 # example: python3 update_opponent_strats.py sl29 sl29_randNoAndB 15
 if __name__ == "__main__":
