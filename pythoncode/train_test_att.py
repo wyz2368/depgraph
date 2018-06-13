10ebde8422b5987f5e3f07e500074fdb9b5f4cf9
Mason Wright <masonwright14@gmail.com>
2018-06-12 22:36:53 -0400

    bug fix

--- pythoncode/train_test_att.py
+++ pythoncode/train_test_att.py
@@ -17,19 +17,19 @@ def close_env_process(env_process):
     env_process.kill()
 
 def run_training(env_short_name, new_epoch, env_name_def_net):
-    cmd = "python3 train_dg_java_mlp_att_vs_mixed.py " + env_name_def_net + " " + \
-        env_short_name + " " + str(new_epoch)
+    cmd_list = ["python3", "train_dg_java_mlp_att_vs_mixed.py", env_name_def_net, \
+        env_short_name, str(new_epoch)]
     att_out_name = "attVMixed_" + env_short_name + "_epoch" + str(new_epoch) + ".txt"
     with open(att_out_name, "w") as file:
-        subprocess.call(cmd, stdout=file)
+        subprocess.call(cmd_list, stdout=file)
 
 def run_evaluation(env_short_name, new_epoch, env_name_def_net):
-    cmd = "python3 enjoy_dg_data_vs_mixed_def.py " + env_name_def_net + " " + \
-        env_short_name + " " + str(new_epoch)
+    cmd_list = ["python3", "enjoy_dg_data_vs_mixed_def.py", env_name_def_net, \
+        env_short_name, str(new_epoch)]
     att_out_name_enj = "att_" + env_short_name + "_randNoAndB_epoch" + str(new_epoch) + \
         ".txt"
     with open(att_out_name_enj, "w") as file:
-        subprocess.call(cmd, stdout=file)
+        subprocess.call(cmd_list, stdout=file)
 
 def main(graph_name, env_short_name, new_epoch, env_name_def_net):
     env_process = start_and_return_env_process(graph_name)
