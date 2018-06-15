0b3a43d69866eac8ee936181384235b0c60957b3
Mason Wright <masonwright14@gmail.com>
2018-06-15 11:29:11 -0400

    bug fixes

--- pythoncode/train_test_att.py
+++ pythoncode/train_test_att.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import time
+import os.path
 
 def start_and_return_env_process(graph_name):
     cmd = "exec java -jar ../depgraphpy4jattvseither/depgraphpy4jattvsnetorheuristic.jar " \
@@ -20,6 +21,9 @@ def run_training(env_short_name, new_epoch, env_name_def_net):
     cmd_list = ["python3", "train_dg_java_mlp_att_vs_mixed.py", env_name_def_net, \
         env_short_name, str(new_epoch)]
     att_out_name = "attVMixed_" + env_short_name + "_epoch" + str(new_epoch) + ".txt"
+    if os.path.isfile(att_out_name):
+        print("Skipping: " + att_out_name + " already exists.")
+        return
     with open(att_out_name, "w") as file:
         subprocess.call(cmd_list, stdout=file)
 
@@ -28,6 +32,9 @@ def run_evaluation(env_short_name, new_epoch, env_name_def_net):
         env_short_name, str(new_epoch)]
     att_out_name_enj = "att_" + env_short_name + "_randNoAndB_epoch" + str(new_epoch) + \
         "_enj.txt"
+    if os.path.isfile(att_out_name_enj):
+        print("Skipping: " + att_out_name_enj + " already exists.")
+        return
     with open(att_out_name_enj, "w") as file:
         subprocess.call(cmd_list, stdout=file)
 
--- pythoncode/train_test_def.py
+++ pythoncode/train_test_def.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import time
+import os.path
 
 def start_and_return_env_process(graph_name):
     cmd = "exec java -jar ../depgraphpy4jdefvseither/depgraphpy4jdefvsnetorheuristic.jar " \
@@ -20,6 +21,9 @@ def run_training(env_short_name, new_epoch, env_name_att_net):
     cmd_list = ["python3", "train_dg_java_mlp_def_vs_mixed.py", env_name_att_net, \
         env_short_name, str(new_epoch)]
     def_out_name = "defVMixed_" + env_short_name + "_epoch" + str(new_epoch) + ".txt"
+    if os.path.isfile(def_out_name):
+        print("Skipping: " + def_out_name + " already exists.")
+        return
     with open(def_out_name, "w") as file:
         subprocess.call(cmd_list, stdout=file)
 
@@ -28,6 +32,9 @@ def run_evaluation(env_short_name, new_epoch, env_name_att_net):
         env_short_name, str(new_epoch)]
     def_out_name_enj = "def_" + env_short_name + "_randNoAndB_epoch" + str(new_epoch) + \
         "_enj.txt"
+    if os.path.isfile(def_out_name_enj):
+        print("Skipping: " + def_out_name_enj + " already exists.")
+        return
     with open(def_out_name_enj, "w") as file:
         subprocess.call(cmd_list, stdout=file)
 
