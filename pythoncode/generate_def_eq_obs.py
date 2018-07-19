bce20631492d9f3598f30f24387771c3798bf543
Mason Wright <masonwright14@gmail.com>
2018-07-19 12:14:49 -0400

    bug fix

--- pythoncode/generate_def_eq_obs.py
+++ pythoncode/generate_def_eq_obs.py
@@ -45,35 +45,38 @@ def get_eq_without_heuristics(eq_complete):
         result[strat] = result[strat] / total_weight
     return result
 
-def get_obs_for_network(env_name_att_net, def_strat, episodes):
-    return generate_obs.generate_obs(env_name_att_net, def_strat, episodes)
+def get_obs_for_network(env_name_att_net, def_strat, episodes, obs_per_episode):
+    return generate_obs.generate_obs(env_name_att_net, def_strat, episodes, obs_per_episode)
 
-def get_all_obs(network_to_weight, num_episodes, env_name_att_net):
+def get_all_obs(network_to_weight, num_episodes, obs_per_episode, env_name_att_net):
     result = []
-    for strat, weight in network_to_weight:
+    for strat, weight in network_to_weight.items():
         cur_episodes = int(num_episodes * weight)
         if cur_episodes > 0:
             print("For net: " + strat + ", will get: " + str(cur_episodes))
-            cur_result = get_obs_for_network(env_name_att_net, strat, cur_episodes)
+            cur_result = get_obs_for_network(env_name_att_net, strat, cur_episodes, \
+                obs_per_episode)
             result.extend(cur_result)
     return result
 
-def main(env_name_att_net, tsv_file, num_episodes, out_file_name):
+def main(env_name_att_net, tsv_file, num_episodes, obs_per_episode, out_file_name):
     tsv_strategy = get_eq_from_file(tsv_file)
     network_to_weight = get_eq_without_heuristics(tsv_strategy)
-    observations = get_all_obs(network_to_weight, num_episodes, env_name_att_net)
+    observations = get_all_obs(network_to_weight, num_episodes, obs_per_episode, \
+        env_name_att_net)
     write_observations_to_file(observations, out_file_name)
 
 '''
 python3 generate_def_eq_obs.py DepgraphJavaEnvVsMixedAtt-v0 d30_epoch14_def.tsv \
-    5000 obs_def_e14_vs_att.csv
+    500 10 obs_def_e14_vs_att.csv
 '''
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        raise ValueError("Need 4 args: env_name_att_net, tsv_file, num_episodes, " + \
-            "out_file_name")
+    if len(sys.argv) != 6:
+        raise ValueError("Need 5 args: env_name_att_net, tsv_file, num_episodes, " + \
+            "obs_per_episode, out_file_name")
     ENV_NAME_ATT_NET = sys.argv[1]
     TSV_FILE = sys.argv[2]
     NUM_EPISODES = int(sys.argv[3])
-    OUT_FILE_NAME = sys.argv[4]
-    main(ENV_NAME_ATT_NET, TSV_FILE, NUM_EPISODES, OUT_FILE_NAME)
+    OBS_PER_EPISODE = int(sys.argv[4])
+    OUT_FILE_NAME = sys.argv[5]
+    main(ENV_NAME_ATT_NET, TSV_FILE, NUM_EPISODES, OBS_PER_EPISODE, OUT_FILE_NAME)
--- srg_pythoncode/enjoy_depgraph_data_for_obs_store.py
+++ srg_pythoncode/enjoy_depgraph_data_for_obs_store.py
@@ -33,7 +33,7 @@ def get_net_scope(net_name):
         return "deepq_train"
     return "deepq_train_e" + str(net_num)
 
-def generate_obs(env_name_att_net, def_strat, num_episodes, eps_per_game):
+def generate_obs(env_name_att_net, def_strat, num_episodes, obs_per_episode):
     '''
         Load the network from file, and play games of the depdency
         graph game against opponent.
@@ -67,7 +67,7 @@ def generate_obs(env_name_att_net, def_strat, num_episodes, eps_per_game):
             obs = np.array(obs).reshape(1, obs_len)
             episode_rew += rew
         rewards.append(episode_rew)
-        for _ in range(eps_per_game):
+        for _ in range(obs_per_episode):
             choice_index = random.randint(0, len(cur_episode_observations) - 1)
             selected_observations.append(cur_episode_observations[choice_index])
     mean_reward = np.mean(rewards)
@@ -84,14 +84,14 @@ def generate_obs(env_name_att_net, def_strat, num_episodes, eps_per_game):
 
 '''
 python3 enjoy_depgraph_data_for_obs_store.py DepgraphJavaEnvVsMixedAtt-v0 \
-    depgraph_dq_mlp_rand_epoch14.pkl 400 5
+    depgraph_dq_mlp_rand_epoch14.pkl 100 5
 '''
 if __name__ == '__main__':
     if len(sys.argv) != 5:
         raise ValueError("Need 4 args: env_name_att_net, def_strat, num_episodes, " + \
-            "eps_per_game")
+            "obs_per_episode")
     ENV_NAME_ATT_NET = sys.argv[1]
     DEF_STRAT = sys.argv[2]
     NUM_EPISODES = int(sys.argv[3])
-    EPS_PER_GAME = int(sys.argv[4])
-    generate_obs(ENV_NAME_ATT_NET, DEF_STRAT, NUM_EPISODES, EPS_PER_GAME)
+    OBS_PER_EPISODE = int(sys.argv[4])
+    generate_obs(ENV_NAME_ATT_NET, DEF_STRAT, NUM_EPISODES, OBS_PER_EPISODE)
