3f5374c058c8efcf2c6ee082b2d5ac3f13040c43
Mason Wright <masonwright14@gmail.com>
2018-07-24 14:50:13 -0400

    adding fine tuning code

--- pythoncode/train_dg_java_mlp_att_and_retrain.py
+++ pythoncode/train_dg_java_mlp_att_and_retrain.py
@@ -11,18 +11,18 @@ from baselines import deepq
 
 PORT_DIR = "../gym/gym/gym/envs/board_game/"
 
-def unlock_train_def(port_lock_name):
-    lock_name = PORT_DIR + port_lock_name + "_train_def_lock.txt"
+def unlock_train_att(port_lock_name):
+    lock_name = PORT_DIR + port_lock_name + "_train_att_lock.txt"
     with open(lock_name, 'w') as file:
         file.write("0\n")
 
-def main(env_name, env_short_name, new_epoch, def_port, port_lock_name, env_short_name_tsv, \
-    max_timesteps_def_init, max_timesteps_def_retrain):
+def main(env_name, env_short_name, new_epoch, att_port, port_lock_name, env_short_name_tsv, \
+    max_timesteps_att_init, max_timesteps_att_retrain):
     '''
     Makes the depgraph environment, builds a multilayer perceptron model,
     trains the model, and saves the result.
     '''
-    model_name = "dg_" + env_short_name + "_dq_mlp_rand_epoch_" + str(new_epoch) + ".pkl"
+    model_name = "dg_" + env_short_name + "_dq_mlp_rand_epoch_" + str(new_epoch) + "_att.pkl"
     if os.path.isfile(model_name):
         raise ValueError("Skipping: " + model_name + " already exists.")
 
@@ -30,12 +30,12 @@ def main(env_name, env_short_name, new_epoch, def_port, port_lock_name, env_shor
 
     start = time.time()
     env = gym.make(env_name)
-    if env.get_port() != def_port:
+    if env.get_port() != att_port:
         raise ValueError("Wrong port: " + str(env.get_port()) + " vs. " + str(def_port))
-    unlock_train_def(port_lock_name)
+    unlock_train_att(port_lock_name)
 
-    strat_file = env_short_name_tsv + "_epoch" + str(new_epoch) + "_att.tsv"
-    env.setup_att_mixed_strat(strat_file)
+    strat_file = env_short_name_tsv + "_epoch" + str(new_epoch) + "_def.tsv"
+    env.setup_def_mixed_strat(strat_file)
 
     model = deepq.models.mlp([256, 256])
 
@@ -44,12 +44,12 @@ def main(env_name, env_short_name, new_epoch, def_port, port_lock_name, env_shor
         my_scope = "deepq_train_e" + str(new_epoch)
 
     prefix_for_save = "dg_" + env_short_name + "_dq_mlp_retrain_epoch" + str(new_epoch)
-    retrain_config_str = env_short_name_tsv + "_epoch" + str(new_epoch) + "_retrain_att.tsv"
+    retrain_config_str = env_short_name_tsv + "_epoch" + str(new_epoch) + "_retrain_def.tsv"
     deepq.learn_retrain_and_save(
         env,
         q_func=model,
         lr=5e-5,
-        max_timesteps_init=max_timesteps_def_init,
+        max_timesteps_init=max_timesteps_att_init,
         buffer_size=3000,
         exploration_fraction=0.5,
         exploration_final_eps=0.03,
@@ -63,7 +63,7 @@ def main(env_name, env_short_name, new_epoch, def_port, port_lock_name, env_shor
         retrain_exploration_initial_eps=0.3,
         retrain_exploration_final_eps=0.03,
         retrain_save_count=3,
-        max_timesteps_retrain=max_timesteps_def_retrain,
+        max_timesteps_retrain=max_timesteps_att_retrain,
         retrain_config_str=retrain_config_str,
         prefix_for_save=prefix_for_save
     )
@@ -76,16 +76,16 @@ def main(env_name, env_short_name, new_epoch, def_port, port_lock_name, env_shor
 
 if __name__ == '__main__':
     if len(sys.argv) != 9:
-        raise ValueError("Need 8 args: env_name_mixed_att, env_short_name, new_epoch, " + \
-            "def_port, port_lock_name, env_short_name_tsv, max_timesteps_def_init, " + \
-            "max_timesteps_def_retrain")
+        raise ValueError("Need 8 args: env_name_mixed_def, env_short_name, new_epoch, " + \
+            "att_port, port_lock_name, env_short_name_tsv, max_timesteps_att_init, " + \
+            "max_timesteps_att_retrain")
     ENV_NAME = sys.argv[1]
     ENV_SHORT_NAME = sys.argv[2]
     NEW_EPOCH = int(sys.argv[3])
-    DEF_PORT = int(sys.argv[4])
+    ATT_PORT = int(sys.argv[4])
     PORT_LOCK_NAME = sys.argv[5]
     ENV_SHORT_NAME_TSV = sys.argv[6]
-    MAX_TIMESTEPS_DEF_INIT = int(sys.argv[7])
-    MAX_TIMESTEPS_DEF_RETRAIN = int(sys.argv[8])
-    main(ENV_NAME, ENV_SHORT_NAME, NEW_EPOCH, DEF_PORT, PORT_LOCK_NAME, ENV_SHORT_NAME_TSV, \
-        MAX_TIMESTEPS_DEF_INIT, MAX_TIMESTEPS_DEF_RETRAIN)
+    MAX_TIMESTEPS_ATT_INIT = int(sys.argv[7])
+    MAX_TIMESTEPS_ATT_RETRAIN = int(sys.argv[8])
+    main(ENV_NAME, ENV_SHORT_NAME, NEW_EPOCH, ATT_PORT, PORT_LOCK_NAME, ENV_SHORT_NAME_TSV, \
+        MAX_TIMESTEPS_ATT_INIT, MAX_TIMESTEPS_ATT_RETRAIN)
