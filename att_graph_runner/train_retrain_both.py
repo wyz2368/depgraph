db3d9252b2f3e7c8706630503e5382e71e39a4bb
Mason Wright <masonwright14@gmail.com>
2018-08-01 20:45:42 -0700

    updating code for curve training

--- att_graph_runner/train_retrain_both.py
+++ att_graph_runner/train_retrain_both.py
@@ -10,8 +10,8 @@ PORT_DIR = "../gym/gym/gym/envs/board_game/"
 
 def get_lines(file_name):
     lines = None
-    with open(file_name) as f:
-        lines = f.readlines()
+    with open(file_name) as my_file:
+        lines = my_file.readlines()
     lines = [x.strip() for x in lines]
     lines = [x for x in lines if x]
     return lines
@@ -131,11 +131,13 @@ def close_env_process(env_process):
     time.sleep(sleep_sec)
     env_process.kill()
 
-def run_training_def(env_short_name, new_epoch, env_name_vs_att, def_port, port_lock_name, \
-    env_short_name_tsv, max_timesteps_def):
-    cmd_list = ["python3", "train_dg_java_mlp_def_vs_mixed.py", env_name_vs_att, \
+def run_train_retrain_def(env_short_name, new_epoch, env_name_vs_att, def_port, \
+    port_lock_name, env_short_name_tsv, max_timesteps_def_init, max_timesteps_def_retrain, \
+    retrain_iters):
+    cmd_list = ["python3", "train_dg_java_mlp_def_and_retrain.py", env_name_vs_att, \
         env_short_name, str(new_epoch), str(def_port), str(port_lock_name), \
-        env_short_name_tsv, str(max_timesteps_def)]
+        env_short_name_tsv, str(max_timesteps_def_init), str(max_timesteps_def_retrain), \
+        str(retrain_iters)]
     def_out_name = "defVMixed_" + env_short_name + "_epoch" + str(new_epoch) + ".txt"
     if os.path.isfile(def_out_name):
         print("Skipping: " + def_out_name + " already exists.")
@@ -147,11 +149,13 @@ def run_training_def(env_short_name, new_epoch, env_name_vs_att, def_port, port_
     def_process = subprocess.Popen(cmd_list, stdout=my_file, stderr=None)
     return def_process, my_file
 
-def run_training_att(env_short_name, new_epoch, env_name_vs_def, att_port, port_lock_name, \
-    env_short_name_tsv, max_timesteps_att):
-    cmd_list = ["python3", "train_dg_java_mlp_att_vs_mixed.py", env_name_vs_def, \
+def run_train_retrain_att(env_short_name, new_epoch, env_name_vs_def, att_port, \
+    port_lock_name, env_short_name_tsv, max_timesteps_att_init, max_timesteps_att_retrain, \
+    retrain_iters):
+    cmd_list = ["python3", "train_dg_java_mlp_att_and_retrain.py", env_name_vs_def, \
         env_short_name, str(new_epoch), str(att_port), str(port_lock_name), \
-        env_short_name_tsv, str(max_timesteps_att)]
+        env_short_name_tsv, str(max_timesteps_att_init), str(max_timesteps_att_retrain), \
+        str(retrain_iters)]
     att_out_name = "attVMixed_" + env_short_name + "_epoch" + str(new_epoch) + ".txt"
     if os.path.isfile(att_out_name):
         print("Skipping: " + att_out_name + " already exists.")
@@ -163,48 +167,62 @@ def run_training_att(env_short_name, new_epoch, env_name_vs_def, att_port, port_
     att_process = subprocess.Popen(cmd_list, stdout=my_file, stderr=None)
     return att_process, my_file
 
-def run_evaluation_def(env_short_name, new_epoch, env_name_vs_att, def_port, \
-    port_lock_name, env_short_name_tsv):
-    cmd_list = ["python3", "enjoy_depgraph_data_vs_mixed.py", env_name_vs_att, \
-        env_short_name, str(new_epoch), str(def_port), str(port_lock_name), \
-        env_short_name_tsv]
-    def_out_name_enj = "def_" + env_short_name + "_randNoAndB_epoch" + str(new_epoch) + \
-        "_enj.txt"
-    if os.path.isfile(def_out_name_enj):
-        print("Skipping: " + def_out_name_enj + " already exists.")
-        unlock_eval_def(port_lock_name)
-        return None, None
+def run_evaluation_all_def(env_short_name, new_epoch, env_name_att_net, def_port, \
+    port_lock_name, env_short_name_tsv, retrain_iters):
+    is_train = False
+    is_retrain_opponent_options = [True, False]
+    for retrain_number in range(retrain_iters + 1):
+        for is_retrain_opponent in is_retrain_opponent_options:
+            wait_for_def_lock(port_lock_name, is_train)
+            lock_def(port_lock_name, is_train)
+            write_def_port(port_lock_name, is_train, def_port)
+            cmd_list = ["python3", "enjoy_depgraph_data_vs_mixed_retraining.py",
+                        env_name_att_net, env_short_name, str(new_epoch), \
+                        str(retrain_number), str(def_port), str(port_lock_name), \
+                        env_short_name_tsv, str(is_retrain_opponent)]
+            def_out_name_enj = "def_" + env_short_name + "_randNoAndB_epoch" + \
+                str(new_epoch) + "_r" + str(retrain_number) + "_enj"
+            if is_retrain_opponent:
+                def_out_name_enj += "_vsRetrain.txt"
+            else:
+                def_out_name_enj += "_vsEq.txt"
+            if os.path.isfile(def_out_name_enj):
+                print("Skipping: " + def_out_name_enj + " already exists.")
+                unlock_eval_def(port_lock_name)
+                continue
+            with open(def_out_name_enj, "w") as file:
+                subprocess.call(cmd_list, stdout=file)
 
-    def_process = None
-    my_file = open(def_out_name_enj, "w")
-    def_process = subprocess.Popen(cmd_list, stdout=my_file, stderr=None)
-    return def_process, my_file
-
-def run_evaluation_att(env_short_name, new_epoch, env_name_vs_def, att_port, \
-    port_lock_name, env_short_name_tsv):
-    cmd_list = ["python3", "enjoy_dg_data_vs_mixed_def.py", env_name_vs_def, \
-        env_short_name, str(new_epoch), str(att_port), str(port_lock_name), \
-        env_short_name_tsv]
-    att_out_name_enj = "att_" + env_short_name + "_randNoAndB_epoch" + str(new_epoch) + \
-        "_enj.txt"
-    if os.path.isfile(att_out_name_enj):
-        print("Skipping: " + att_out_name_enj + " already exists.")
-        unlock_eval_att(port_lock_name)
-        return None, None
-
-    att_process = None
-    my_file = open(att_out_name_enj, "w")
-    att_process = subprocess.Popen(cmd_list, stdout=my_file, stderr=None)
-    return att_process, my_file
-
-def run_retrain_both(graph_name, env_short_name_payoffs, new_epoch, \
-    env_name_vs_mixed_att, env_name_vs_mixed_def, port_lock_name, env_short_name_tsv, \
-    max_timesteps_def_init, max_timesteps_def_retrain, max_timesteps_att_init, \
-    max_timesteps_att_retrain, save_count):
-    pass
+def run_evaluation_all_att(env_short_name, new_epoch, env_name_def_net, att_port, \
+    port_lock_name, env_short_name_tsv, retrain_iters):
+    is_train = False
+    is_retrain_opponent_options = [True, False]
+    for retrain_number in range(retrain_iters + 1):
+        for is_retrain_opponent in is_retrain_opponent_options:
+            wait_for_att_lock(port_lock_name, is_train)
+            lock_att(port_lock_name, is_train)
+            write_att_port(port_lock_name, is_train, att_port)
+            cmd_list = ["python3", "enjoy_dg_data_vs_mixed_def_retraining.py",
+                        env_name_def_net, env_short_name, str(new_epoch), \
+                        str(retrain_number), str(att_port), str(port_lock_name), \
+                        env_short_name_tsv, str(is_retrain_opponent)]
+            att_out_name_enj = "att_" + env_short_name + "_randNoAndB_epoch" + \
+                str(new_epoch) + "_r" + str(retrain_number) + "_enj"
+            if is_retrain_opponent:
+                att_out_name_enj += "_vsRetrain.txt"
+            else:
+                att_out_name_enj += "_vsEq.txt"
+            if os.path.isfile(att_out_name_enj):
+                print("Skipping: " + att_out_name_enj + " already exists.")
+                unlock_eval_att(port_lock_name)
+                continue
+            with open(att_out_name_enj, "w") as file:
+                subprocess.call(cmd_list, stdout=file)
 
 def run_both(graph_name, env_short_name, new_epoch, env_name_vs_att, env_name_vs_def, \
-    port_lock_name, def_port, env_short_name_tsv, max_timesteps_def, max_timesteps_att):
+             port_lock_name, def_port, env_short_name_tsv, max_timesteps_def_init, \
+             max_timesteps_def_retrain, max_timesteps_att_init, \
+             max_timesteps_att_retrain, retrain_iters):
 
     ### Setup
 
@@ -217,14 +235,16 @@ def run_both(graph_name, env_short_name, new_epoch, env_name_vs_att, env_name_vs
     is_train = True
 
     write_def_port(port_lock_name, is_train, def_port)
-    def_process, def_file = run_training_def(env_short_name, new_epoch, env_name_vs_att, \
-        def_port, port_lock_name, env_short_name_tsv, max_timesteps_def)
+    def_process, def_file = run_train_retrain_def(env_short_name, new_epoch, \
+        env_name_vs_att, def_port, port_lock_name, env_short_name_tsv, \
+        max_timesteps_def_init, max_timesteps_def_retrain, retrain_iters)
 
     wait_for_att_lock(port_lock_name, is_train)
     lock_att(port_lock_name, is_train)
     write_att_port(port_lock_name, is_train, att_port)
-    att_process, att_file = run_training_att(env_short_name, new_epoch, env_name_vs_def, \
-        att_port, port_lock_name, env_short_name_tsv, max_timesteps_att)
+    att_process, att_file = run_train_retrain_att(env_short_name, new_epoch, \
+        env_name_vs_def, att_port, port_lock_name, env_short_name_tsv, \
+        max_timesteps_att_init, max_timesteps_att_retrain, retrain_iters)
 
     if def_process is not None:
         print("Waiting for def training")
@@ -240,26 +260,11 @@ def run_both(graph_name, env_short_name, new_epoch, env_name_vs_att, env_name_vs
 
     is_train = False
 
-    wait_for_def_lock(port_lock_name, is_train)
-    lock_def(port_lock_name, is_train)
-    write_def_port(port_lock_name, is_train, def_port)
-    def_process_enj, def_file_enj = run_evaluation_def(env_short_name, new_epoch, \
-        env_name_vs_att, def_port, port_lock_name, env_short_name_tsv)
+    run_evaluation_all_def(env_short_name, new_epoch, \
+        env_name_vs_att, def_port, port_lock_name, env_short_name_tsv, retrain_iters)
 
-    wait_for_att_lock(port_lock_name, is_train)
-    lock_att(port_lock_name, is_train)
-    write_att_port(port_lock_name, is_train, att_port)
-    att_process_enj, att_file_enj = run_evaluation_att(env_short_name, new_epoch, \
-        env_name_vs_def, att_port, port_lock_name, env_short_name_tsv)
-
-    if def_process_enj is not None:
-        print("Waiting for def evaluation")
-        def_process_enj.wait()
-        def_file_enj.close()
-    if att_process_enj is not None:
-        print("Waiting for att evaluation")
-        att_process_enj.wait()
-        att_file_enj.close()
+    run_evaluation_all_att(env_short_name, new_epoch, \
+        env_name_vs_def, att_port, port_lock_name, env_short_name_tsv, retrain_iters)
 
     ### Takedown
 
@@ -269,15 +274,16 @@ def run_both(graph_name, env_short_name, new_epoch, env_name_vs_att, env_name_vs
     print("Finished defender train and test")
 
 '''
-example: python3 train_test_both.py SepLayerGraph0_noAnd_B.json sl29 16 \
+example: python3 train_retrain_both.py SepLayerGraph0_noAnd_B.json sl29 16 \
     DepgraphJavaEnvVsMixedAtt29N-v0 DepgraphJavaEnvVsMixedDef29N-v0 s29 25333 \
-    sl29_randNoAndB 700000 700000
+    sl29_randNoAndB 700000 400000 700000 400000 3
 '''
 if __name__ == '__main__':
-    if len(sys.argv) != 11:
-        raise ValueError("Need 10 args: graph_name, env_short_name, new_epoch, " + \
+    if len(sys.argv) != 14:
+        raise ValueError("Need 13 args: graph_name, env_short_name, new_epoch, " + \
             "env_name_vs_att, env_name_vs_def, port_lock_name, def_port " + \
-            "env_short_name_tsv, max_timesteps_def, max_timesteps_att")
+            "env_short_name_tsv, max_timesteps_def_init, max_timesteps_def_retrain, " + \
+            "max_timesteps_att_init, max_timesteps_att_retrain, retrain_iters")
     GRAPH_NAME = sys.argv[1]
     ENV_SHORT_NAME = sys.argv[2]
     NEW_EPOCH = int(sys.argv[3])
@@ -286,7 +292,12 @@ if __name__ == '__main__':
     PORT_LOCK_NAME = sys.argv[6]
     DEF_PORT = int(sys.argv[7])
     ENV_SHORT_NAME_TSV = sys.argv[8]
-    MAX_TIMESTEPS_DEF = int(sys.argv[9])
-    MAX_TIMESTEPS_ATT = int(sys.argv[10])
+    MAX_TIMESTEPS_DEF_INIT = int(sys.argv[9])
+    MAX_TIMESTEPS_DEF_RETRAIN = int(sys.argv[10])
+    MAX_TIMESTEPS_ATT_INIT = int(sys.argv[11])
+    MAX_TIMESTEPS_ATT_RETRAIN = int(sys.argv[12])
+    RETRAIN_ITERS = int(sys.argv[13])
     run_both(GRAPH_NAME, ENV_SHORT_NAME, NEW_EPOCH, ENV_NAME_VS_ATT, ENV_NAME_VS_DEF, \
-        PORT_LOCK_NAME, DEF_PORT, ENV_SHORT_NAME_TSV, MAX_TIMESTEPS_DEF, MAX_TIMESTEPS_ATT)
+        PORT_LOCK_NAME, DEF_PORT, ENV_SHORT_NAME_TSV, MAX_TIMESTEPS_DEF_INIT, \
+        MAX_TIMESTEPS_DEF_RETRAIN, MAX_TIMESTEPS_ATT_INIT, MAX_TIMESTEPS_ATT_RETRAIN, \
+        RETRAIN_ITERS)
