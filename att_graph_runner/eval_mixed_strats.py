79e22caf07ee2b77357946bc1157a9221576b633
Mason Wright <masondw@umich.edu>
2018-09-10 20:25:44 -0400

    fixing example args

--- att_graph_runner/eval_mixed_strats.py
+++ att_graph_runner/eval_mixed_strats.py
@@ -176,7 +176,8 @@ def main(env_name_def_net, env_name_att_net, env_name_both, game_file, num_sims,
 
 '''
 example: python3 eval_mixed_strats.py DepgraphJava-v0 DepgraphJavaEnvAtt-v0 \
-    DepgraphJavaEnvBoth-v0 1000 game_3014.json 1 RandomGraph30N100E6T1_B.json True
+    DepgraphJavaEnvBoth-v0 game_3014.json 1000 d30_epoch24_att.tsv \
+    d30cd1_randNoAndB_epoch21_def.tsv RandomGraph30N100E6T1_B.json True
 '''
 if __name__ == '__main__':
     if len(sys.argv) != 10:
