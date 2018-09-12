3b623988b3bd63d27670edb0faacbc7c16dfc8bc
Mason Wright <masondw@umich.edu>
2018-09-12 14:16:37 -0400

    adding code to union two run results

---
+++ att_graph_runner/union_games.py
@@ -0,0 +1,185 @@
+import sys
+import os.path
+import copy
+import time
+from get_both_payoffs_from_game import get_json_data
+from check_game_data import check_for_missing_payoffs, check_for_extra_payoffs, \
+    check_for_duplicates, get_attacker_strats, get_defender_strats
+from eval_mixed_strats import hybrid_get_net_scope, is_network
+from cli_enjoy_dg_two_sided import get_payoffs_both
+from generate_new_cols import print_json
+from add_new_data import add_defender_strategy, add_attacker_strategy, get_max_profile_id, \
+    get_max_symmetry_group_id, get_original_num_sims, add_profile
+
+def is_game_data_valid(game_data):
+    no_missing = check_for_missing_payoffs(game_data)
+    no_extra = check_for_extra_payoffs(game_data)
+    no_dups = check_for_duplicates(game_data)
+    return no_dups and no_missing and no_extra
+
+def get_attacker_networks(game_data):
+    return [x for x in get_attacker_strats(game_data) if is_network(x)]
+
+def get_defender_networks(game_data):
+    return [x for x in get_defender_strats(game_data) if is_network(x)]
+
+def get_attacker_heuristics(game_data):
+    return [x for x in get_attacker_strats(game_data) if not is_network(x)]
+
+def get_defender_heuristics(game_data):
+    return [x for x in get_defender_strats(game_data) if not is_network(x)]
+
+def generate_new_payoffs(att_nets_a, def_nets_a, att_nets_b, def_nets_b, num_sims, \
+    env_name_both, graph_name, old_game_file_a, old_game_file_b):
+    result = {}
+    result["old_game_file_a"] = old_game_file_a
+    result["old_game_file_b"] = old_game_file_b
+    result["env_name_both"] = env_name_both
+    result["num_sims"] = num_sims
+    result["graph_name"] = graph_name
+    start_time = time.time()
+
+    for att_net in att_nets_a:
+        for def_net in def_nets_b:
+            mean_rewards_tuple = get_payoffs_both( \
+                env_name_both, num_sims, def_net, att_net, graph_name, \
+                hybrid_get_net_scope(def_net), hybrid_get_net_scope(att_net))
+            print(str((def_net, att_net)) + "\n" + str(mean_rewards_tuple))
+            result[def_net][att_net] = list(mean_rewards_tuple)
+    for att_net in att_nets_b:
+        for def_net in def_nets_a:
+            mean_rewards_tuple = get_payoffs_both( \
+                env_name_both, num_sims, def_net, att_net, graph_name, \
+                hybrid_get_net_scope(def_net), hybrid_get_net_scope(att_net))
+            print(str((def_net, att_net)) + "\n" + str(mean_rewards_tuple))
+            result[def_net][att_net] = list(mean_rewards_tuple)
+    duration = time.time() - start_time
+    print("Minutes taken: " + str(duration // 60))
+    return result
+
+def get_results_to_add(new_payoffs_dict, att_nets_a, def_nets_a, att_nets_b, def_nets_b):
+    result = []
+    for att_net in att_nets_a:
+        for def_net in def_nets_b:
+            (mean_def_reward, mean_att_reward) = new_payoffs_dict[def_net][att_net]
+            result.append((def_net, att_net, mean_def_reward, mean_att_reward))
+    for att_net in att_nets_b:
+        for def_net in def_nets_a:
+            (mean_def_reward, mean_att_reward) = new_payoffs_dict[def_net][att_net]
+            result.append((def_net, att_net, mean_def_reward, mean_att_reward))
+    return result
+
+def get_payoffs(game_data, defender, attacker):
+    '''
+    Returns True iff there is a payoff entry for the defender-attacker pair.
+    '''
+    for profile in game_data["profiles"]:
+        matches_def = False
+        matches_att = False
+        def_payoff = None
+        att_payoff = None
+        for group in profile["symmetry_groups"]:
+            if group["strategy"] == defender and group["role"] == "defender":
+                matches_def = True
+                def_payoff = group["payoff"]
+            if group["strategy"] == attacker and group["role"] == "attacker":
+                matches_att = True
+                att_payoff = group["payoff"]
+        if matches_def and matches_att:
+            return (def_payoff, att_payoff)
+    raise ValueError("Payoff not found: " + defender + "\t" + attacker)
+
+def get_game_b_results_to_add(game_data_b, att_nets_b, def_nets_b, att_heuristics, \
+    def_heuristics):
+    result = []
+    for att_net in att_nets_b:
+        for def_strat in def_heuristics:
+            (mean_def_reward, mean_att_reward) = get_payoffs(game_data_b, def_strat, \
+                att_net)
+            result.append((def_strat, att_net, mean_def_reward, mean_att_reward))
+    for att_strat in att_heuristics:
+        for def_net in def_nets_b:
+            (mean_def_reward, mean_att_reward) = get_payoffs(game_data_b, def_net, \
+                att_strat)
+            result.append((def_net, att_strat, mean_def_reward, mean_att_reward))
+    return result
+
+def union_game_data(game_data_a, game_data_b, att_nets_a, def_nets_a, att_nets_b, \
+    def_nets_b, att_heuristics, def_heuristics, new_payoffs_dict):
+    result = copy.deepcopy(game_data_a)
+    for def_net in def_nets_b:
+        add_defender_strategy(result, def_net)
+    for att_net in att_nets_b:
+        add_attacker_strategy(result, att_net)
+
+    original_num_sims = get_original_num_sims(result)
+    new_num_sims = new_payoffs_dict["num_sims"]
+    next_profile_id = get_max_profile_id(result) + 1
+    next_symmetry_group_id = get_max_symmetry_group_id(result) + 1
+    results_to_add = get_results_to_add(new_payoffs_dict, att_nets_a, def_nets_a, \
+        att_nets_b, def_nets_b)
+    results_to_add.extend(get_game_b_results_to_add(game_data_b, att_nets_b, def_nets_b, \
+        att_heuristics, def_heuristics))
+    for result_to_add in results_to_add:
+        (def_strat, att_strat, def_payoff, att_payoff) = result_to_add
+        if def_strat in result and att_strat in result[def_strat]:
+            print("Danger: duplicate profile: " + def_strat + " " + att_strat)
+            sys.exit(1)
+        add_profile(result, def_strat, att_strat, def_payoff, att_payoff, \
+                    next_profile_id, next_symmetry_group_id, \
+                    original_num_sims, new_num_sims)
+        next_profile_id += 1
+        next_symmetry_group_id += 2
+    return result
+
+def main(old_game_file_a, old_game_file_b, runs_per_pair, env_name_both, graph_name, \
+    result_payoffs_file, result_game_file):
+    game_data_a = get_json_data(old_game_file_a)
+    game_data_b = get_json_data(old_game_file_b)
+    if not is_game_data_valid(game_data_a) or not is_game_data_valid(game_data_b):
+        raise ValueError("Invalid game data")
+
+    if os.path.isfile(result_payoffs_file):
+        raise ValueError("File already exists: " + result_payoffs_file)
+    if os.path.isfile(result_game_file):
+        raise ValueError("File already exists: " + result_game_file)
+
+    att_nets_a = get_attacker_networks(game_data_a)
+    def_nets_a = get_defender_networks(game_data_a)
+    att_nets_b = get_attacker_networks(game_data_b)
+    def_nets_b = get_defender_networks(game_data_b)
+
+    new_payoffs_dict = generate_new_payoffs(att_nets_a, def_nets_a, att_nets_b, \
+        def_nets_b, runs_per_pair, env_name_both, graph_name, old_game_file_a, \
+        old_game_file_b)
+    print_json(result_payoffs_file, new_payoffs_dict)
+
+    if not os.path.isfile(result_payoffs_file):
+        raise ValueError(result_payoffs_file + " missing.")
+    new_payoffs_dict = get_json_data(result_payoffs_file)
+
+    att_heuristics = get_attacker_heuristics(game_data_a)
+    def_heuristics = get_defender_heuristics(game_data_a)
+
+    combined_game_data = union_game_data(game_data_a, game_data_b, att_nets_a, def_nets_a, \
+        att_nets_b, def_nets_b, att_heuristics, def_heuristics, new_payoffs_dict)
+    print_json(result_game_file, combined_game_data)
+
+'''
+example: python3 union_games.py game_3014_20_d30cd1.json game_3014_23.json \
+   400 DepgraphJavaEnvBoth-v0 RandomGraph30N100E6T1_B.json game_3014_comb_d30cd1_d30n1.json
+'''
+if __name__ == '__main__':
+    if len(sys.argv) != 8:
+        raise ValueError("Need 7 args: old_game_file_a, old_game_file_b, " + \
+                         "runs_per_pair, env_name_both, graph_name, " + \
+                         "result_payoffs_file, result_game_file")
+    OLD_GAME_FILE_A = sys.argv[1]
+    OLD_GAME_FILE_B = sys.argv[2]
+    RUNS_PER_PAIR = int(sys.argv[3])
+    ENV_NAME_BOTH = sys.argv[4]
+    GRAPH_NAME = sys.argv[5]
+    RESULT_PAYOFFS_FILE = sys.argv[6]
+    RESULT_GAME_FILE = sys.argv[7]
+    main(OLD_GAME_FILE_A, OLD_GAME_FILE_B, RUNS_PER_PAIR, ENV_NAME_BOTH, GRAPH_NAME, \
+        RESULT_PAYOFFS_FILE, RESULT_GAME_FILE)
