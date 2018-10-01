49b2e16da731918559f6940cc0325603e1971188
Mason Wright <masondw@umich.edu>
2018-10-01 18:52:53 -0400

    changing to analyze all MSNEs

---
+++ att_graph_runner/decode_gambit_solution_all.py
@@ -0,0 +1,112 @@
+'''
+Decode the output of Gambit's LCP solver for mixed-strategy Nash equilibria,
+by printing out the corresponding strategy name for each weight in the mixed strategy.
+
+Takes as input the LCP solution from Gambit, and a 2-player .nfg (outcome version)
+game file.
+See also: http://www.gambit-project.org/gambit13/formats.html
+'''
+
+import sys
+
+def get_all_mixture_weights(gambit_solution_string):
+    '''
+    Return the mixed strategy weights from the "LCP" solution method of Gambit.
+    '''
+    all_values = gambit_solution_string.splitlines()
+    result = []
+    for values in all_values:
+        if "," in values:
+            values = values.strip().split(",")
+            values.pop(0)
+            values = [float(x) for x in values]
+            result.append(values)
+    return result
+
+def print_all_outputs(attacker_strategies, defender_strategies, is_attacker_first, \
+    all_mixture_weights):
+    '''
+    Print the corresponding strategy name with each mixture weight, for the given mixed
+    strategy.
+    '''
+    if len(attacker_strategies) + len(defender_strategies) != len(all_mixture_weights[0]):
+        raise ValueError("Wrong numbre of mixture weights: " + \
+            str(len(all_mixture_weights[0])) + \
+            ", expected " + str(len(attacker_strategies) + len(defender_strategies)))
+    attacker_offset = 0 if is_attacker_first else len(defender_strategies)
+    for mixture_weights in all_mixture_weights:
+        print("Attacker mixed strategy:")
+        for i in range(len(attacker_strategies)):
+            cur_weight = mixture_weights[i + attacker_offset]
+            cur_attacker = attacker_strategies[i]
+            if cur_weight > 0.0:
+                print(cur_attacker + "\t" + str(cur_weight))
+        print("\nDefender mixed strategy:")
+        defender_offset = len(attacker_strategies) if is_attacker_first else 0
+        for i in range(len(defender_strategies)):
+            cur_weight = mixture_weights[i + defender_offset]
+            cur_defender = defender_strategies[i]
+            if cur_weight > 0.0:
+                print(cur_defender + "\t" + str(cur_weight))
+        print("\n###")
+
+def get_profile_names(gambit_string):
+    '''
+    Given the text of the game file, return the lists of attacker and defender
+    strategy names in order, and whether the attacker is listed as the first player.
+    '''
+    first_open_curly = gambit_string.find("{")
+    second_open_curly = gambit_string.find("{", first_open_curly + 1)
+
+    is_attacker_first = (gambit_string.find("attacker") < gambit_string.find("defender"))
+
+    third_open_curly = gambit_string.find("{", second_open_curly + 1)
+    next_close_curly = gambit_string.find("}", third_open_curly + 1)
+    first_inner = gambit_string[third_open_curly + 1 : next_close_curly]
+    first_inner = first_inner.strip()
+    first_names = first_inner.split()
+    first_names = [x.replace("\"", "") for x in first_names]
+
+    fourth_open_curly = gambit_string.find("{", next_close_curly + 1)
+    next_close_curly = gambit_string.find("}", fourth_open_curly + 1)
+    second_inner = gambit_string[fourth_open_curly + 1 : next_close_curly]
+    second_inner = second_inner.strip()
+    second_names = second_inner.split()
+    second_names = [x.replace("\"", "") for x in second_names]
+
+    attacker_strings = first_names
+    defender_strings = second_names
+    if not is_attacker_first:
+        defender_strings = first_names
+        attacker_strings = second_names
+    return (attacker_strings, defender_strings, is_attacker_first)
+
+def get_file_text(file_name):
+    '''
+    Return the file's text as one string.
+    '''
+    with open(file_name, 'r') as my_file:
+        data = my_file.read()
+        return data
+
+def main(game_file_name, solution_file_name):
+    '''
+    Load the lists of attacker and defender strategy names, and whether the attacker is
+    listed first, from the game file.
+    Load the list of mixed strategy weights from the solution file.
+    Then print the corresponding strategy names for all non-zero weights in the solution.
+    '''
+    gambit_data = get_file_text(game_file_name)
+    (attacker_strings, defender_strings, is_attacker_first) = get_profile_names(gambit_data)
+
+    solution_data = get_file_text(solution_file_name)
+    all_mixture_weights = get_all_mixture_weights(solution_data)
+    print_all_outputs(attacker_strings, defender_strings, is_attacker_first, \
+        all_mixture_weights)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        raise ValueError("Needs 2 arguments: game_file_name solution_file_name")
+    GAME_FILE_NAME = sys.argv[1]
+    SOLUTION_FILE_NAME = sys.argv[2]
+    main(GAME_FILE_NAME, SOLUTION_FILE_NAME)
--- att_graph_runner/runs_analyze.py
+++ att_graph_runner/runs_analyze.py
@@ -3,7 +3,7 @@ import os.path
 from gambit_analyze import call_and_wait, call_and_wait_with_timeout
 from get_both_payoffs_from_game import get_json_data
 from create_tsv_files import get_defender_lines, get_attacker_lines, \
-    get_rounded_strategy_lines
+    get_rounded_strategy_lines, get_file_lines
 from eval_mixed_strats import is_network
 
 def get_unioned_gambit_input_name(unioned_game_file):
@@ -50,7 +50,7 @@ def call_decode_gambit_solution_unioned(unioned_game_file):
         raise ValueError(gambit_input_name + " missing.")
     if not os.path.isfile(gambit_result_name):
         raise ValueError(gambit_result_name + " missing.")
-    command_str = "python3 decode_gambit_solution.py " + gambit_input_name + " " + \
+    command_str = "python3 decode_gambit_solution_all.py " + gambit_input_name + " " + \
         gambit_result_name + " > " + gambit_decoded_name
     call_and_wait(command_str)
 
@@ -62,6 +62,74 @@ def get_defender_mixed_strat(decoded_result_name):
         parts = line.split("\t")
         result[parts[0]] = float(parts[1])
     return result
+
+def get_defender_lines_all(decoded_result_name):
+    lines = get_file_lines(decoded_result_name)
+    split_indexes = [i for i, x in enumerate(lines) if "###" in x]
+    if not split_indexes:
+        return [get_defender_lines(decoded_result_name)]
+
+    result = []
+    for i in range(len(split_indexes)):
+        if i == 0:
+            cur_min = 0
+        else:
+            cur_min = split_indexes[i - 1] + 1
+        if i == len(split_indexes) - 1:
+            cur_max = len(lines) - 1
+        else:
+            cur_max = split_indexes[i] - 1
+        cur_lines = lines[cur_min:cur_max + 1]
+        first_defender_line = cur_lines.index("Defender mixed strategy:") + 1
+        cur_def_lines = cur_lines[first_defender_line:]
+        result.append(cur_def_lines)
+    return result
+
+def get_attacker_lines_all(decoded_result_name):
+    lines = get_file_lines(decoded_result_name)
+    split_indexes = [i for i, x in enumerate(lines) if "###" in x]
+    if not split_indexes:
+        return [get_attacker_lines(decoded_result_name)]
+
+    result = []
+    for i in range(len(split_indexes)):
+        if i == 0:
+            cur_min = 0
+        else:
+            cur_min = split_indexes[i - 1] + 1
+        if i == len(split_indexes) - 1:
+            cur_max = len(lines) - 1
+        else:
+            cur_max = split_indexes[i] - 1
+        cur_lines = lines[cur_min:cur_max + 1]
+        after_last_attacker_line = cur_lines.index("")
+        cur_att_lines = cur_lines[1:after_last_attacker_line]
+        result.append(cur_att_lines)
+    return result
+
+def get_all_defender_mixed_strats(decoded_result_name):
+    all_lines = get_defender_lines_all(decoded_result_name)
+    all_lines = [get_rounded_strategy_lines(x) for x in all_lines]
+    results = []
+    for lines in all_lines:
+        cur_result = {}
+        for line in lines:
+            parts = line.split("\t")
+            cur_result[parts[0]] = float(parts[1])
+        results.append(cur_result)
+    return results
+
+def get_all_attacker_mixed_strats(decoded_result_name):
+    all_lines = get_attacker_lines_all(decoded_result_name)
+    all_lines = [get_rounded_strategy_lines(x) for x in all_lines]
+    results = []
+    for lines in all_lines:
+        cur_result = {}
+        for line in lines:
+            parts = line.split("\t")
+            cur_result[parts[0]] = float(parts[1])
+        results.append(cur_result)
+    return results
 
 def get_attacker_mixed_strat(decoded_result_name):
     lines = get_attacker_lines(decoded_result_name)
@@ -101,6 +169,24 @@ def print_results(unioned_game_data, defender_mixed_strat, attacker_mixed_strat)
     print("Attacker run to eq fraction:")
     print(attacker_run_to_fraction)
 
+def print_all_results(unioned_game_data, defender_mixed_strats, attacker_mixed_strats):
+    if len(defender_mixed_strats) != len(attacker_mixed_strats):
+        raise ValueError("Lengths must match: " + str(len(defender_mixed_strats)) + ", " + \
+            str(len(attacker_mixed_strats)))
+    for i in range(len(defender_mixed_strats)):
+        defender_run_to_fraction = get_run_fractions(defender_mixed_strats[i], \
+            unioned_game_data)
+        attacker_run_to_fraction = get_run_fractions(attacker_mixed_strats[i], \
+            unioned_game_data)
+
+        print("Defender run to eq fraction:")
+        print(defender_run_to_fraction)
+
+        print("Attacker run to eq fraction:")
+        print(attacker_run_to_fraction)
+
+        print("\n###\n")
+
 def main(unioned_game_file):
     make_union_gambit_file(unioned_game_file)
     gambit_analyze_unioned(unioned_game_file)
@@ -108,9 +194,9 @@ def main(unioned_game_file):
 
     unioned_game_data = get_json_data(unioned_game_file)
     decoded_result_name = get_unioned_decoded_result_name(unioned_game_file)
-    defender_mixed_strat = get_defender_mixed_strat(decoded_result_name)
-    attacker_mixed_strat = get_attacker_mixed_strat(decoded_result_name)
-    print_results(unioned_game_data, defender_mixed_strat, attacker_mixed_strat)
+    defender_mixed_strats = get_all_defender_mixed_strats(decoded_result_name)
+    attacker_mixed_strats = get_all_attacker_mixed_strats(decoded_result_name)
+    print_all_results(unioned_game_data, defender_mixed_strats, attacker_mixed_strats)
 
 '''
 example: python3 runs_analyze.py game_comb_d30cd1_d30n1.json
