290cc9a7a7df7d21b1dce56dd52f696a2aa3ca49
Mason Wright <masondw@umich.edu>
2018-10-04 22:34:48 -0400

    minor update

--- att_graph_runner/plot_increases.py
+++ att_graph_runner/plot_increases.py
@@ -18,6 +18,13 @@ def get_total_gain(prev_round_dict, cur_round_dict, cur_net_name):
                 result += weight
     return result
 
+def get_total_gain_missing_only(prev_round_dict, cur_round_dict, cur_net_name):
+    result = 0.0
+    for strat, weight in cur_round_dict.items():
+        if strat != cur_net_name and strat not in prev_round_dict:
+            result += weight
+    return result
+
 def get_network_round_to_name(round_dict):
     result = {}
     for strat in round_dict.keys():
@@ -26,14 +33,18 @@ def get_network_round_to_name(round_dict):
             result[net_round] = strat
     return result
 
-def get_all_increases(round_dicts):
+def get_all_increases(round_dicts, is_missing_only):
     result = []
     for i in range(1, len(round_dicts)):
         prev_round_dict = round_dicts[i - 1]
         cur_round_dict = round_dicts[i]
         cur_rounds_to_names = get_network_round_to_name(cur_round_dict)
         cur_net_name = cur_rounds_to_names.get(str(i), None)
-        total_gain = get_total_gain(prev_round_dict, cur_round_dict, cur_net_name)
+        if is_missing_only:
+            total_gain = get_total_gain_missing_only(prev_round_dict, cur_round_dict, \
+                cur_net_name)
+        else:
+            total_gain = get_total_gain(prev_round_dict, cur_round_dict, cur_net_name)
         result.append(total_gain)
     return result
 
@@ -76,8 +87,8 @@ def plot_increases(def_increases, att_increases, env_short_name_payoffs):
 
 def main(env_short_name_payoffs, env_short_name_tsv):
     def_eqs, att_eqs = get_all_eq_from_files(env_short_name_tsv)
-    def_increases = get_all_increases(def_eqs)
-    att_increases = get_all_increases(att_eqs)
+    def_increases = get_all_increases(def_eqs, False)
+    att_increases = get_all_increases(att_eqs, False)
 
     print("def increases: " + str(def_increases))
     print("att increases: " + str(att_increases))
@@ -89,6 +100,14 @@ def main(env_short_name_payoffs, env_short_name_tsv):
     print("mean att increase: " + fmt.format(mean_att_increase))
 
     plot_increases(def_increases, att_increases, env_short_name_payoffs)
+
+    def_increases_missing_only = get_all_increases(def_eqs, True)
+    att_increases_missing_only = get_all_increases(att_eqs, True)
+    mean_def_increase_missing_only = np.mean(def_increases_missing_only)
+    mean_att_increase_missing_only = np.mean(att_increases_missing_only)
+    print("mean def increase missing only: " + fmt.format(mean_def_increase_missing_only))
+    print("mean att increase missing only: " + fmt.format(mean_att_increase_missing_only))
+
 
 '''
 example: python3 plot_increases.py s29m1 s29m1_randNoAndB
