e7b2dcc87365f2db89a4a84fef2a22321f9c0b64
Mason Wright <masondw@umich.edu>
2018-10-08 19:10:48 -0400

    bug fix in sem computation

---
+++ att_graph_runner/plot_mean_gains_stderror.py
@@ -0,0 +1,98 @@
+import sys
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
+from pylab import savefig
+from plot_mean_payoffs import get_all_mean_gains_with_standard_errors
+
+def get_env_short_name_tsv(env_short_name_payoffs):
+    if env_short_name_payoffs in ["s29n1", "d30n1"]:
+        return env_short_name_payoffs
+    return env_short_name_payoffs + "_randNoAndB"
+
+def plot_gains_with_stderr(def_gains, att_gains, def_errs, att_errs, \
+    env_short_name_payoffs):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(8, 5)
+
+    my_lw = 2
+
+    def_errs = def_errs[:len(def_gains)]
+    plt.errorbar(range(len(def_gains)), def_gains, yerr=def_errs, lw=my_lw, \
+        label='Def. gain', color='blue', elinewidth=1)
+    att_errs = att_errs[:len(att_gains)]
+    plt.errorbar(range(len(att_gains)), att_gains, yerr=att_errs, lw=my_lw, \
+        linestyle='--', label='Att. gain', color='orange', \
+        elinewidth=1)
+
+    ax.axhline(0, color='black', lw=1)
+    ax.axvline(0, color='black', lw=1)
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    plt.ylabel('Mean gain', fontsize=16)
+    plt.xlabel('Training round', fontsize=16)
+    ax.yaxis.set_ticks_position('left')
+    ax.tick_params(labelsize=14)
+    tick_spacing = 2
+    ax.xaxis.set_major_locator(tick.MultipleLocator(tick_spacing))
+    ax.legend()
+    y_max = max(max(def_gains), max(att_gains))
+    ax.set_ylim(-10, y_max + 10)
+    plt.tick_params(
+        axis='x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        top='off'         # ticks along the top edge are off
+    )
+    plt.tick_params(
+        axis='y',          # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        right='off'         # ticks along the right edge are off
+    )
+    plt.tight_layout()
+
+    #plt.show()
+    savefig(env_short_name_payoffs + "_gain_vs_round.pdf")
+
+def combine_stderrs(eq_errors, net_errors):
+    result = []
+    max_len = max(len(eq_errors), len(net_errors))
+    for i in range(max_len):
+        if i < len(eq_errors) and i < len(net_errors):
+            result.append(max(eq_errors[i], net_errors[i]))
+        elif i < len(eq_errors):
+            result.append(eq_errors[i])
+        else:
+            result.append(net_errors[i])
+    return result
+
+def main(game_number, env_short_name_payoffs_list):
+    print("Using environments: " + str(env_short_name_payoffs_list))
+    env_short_name_tsv_list = [get_env_short_name_tsv(x) for x in \
+        env_short_name_payoffs_list]
+    mean_att_gain, mean_def_gain, sem_att_gain, sem_def_gain = \
+        get_all_mean_gains_with_standard_errors(game_number, env_short_name_payoffs_list, \
+        env_short_name_tsv_list)
+    print(mean_att_gain)
+    print(mean_def_gain)
+    print(sem_att_gain)
+    print(sem_def_gain)
+    # print(str(len(att_gains)) + "\t" + str(len(def_gains)))
+    save_env_name = env_short_name_payoffs_list[0] + "_mean_stderr"
+    plot_gains_with_stderr(mean_def_gain, mean_att_gain, sem_def_gain, sem_att_gain, \
+        save_env_name)
+
+'''
+example: python3 plot_mean_gains_stderror.py 3014 d30d1 d30m1 d30n1 d30f1 d30f2
+'''
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        raise ValueError("Need 2+ args: game_number, env_short_name_payoffs+")
+    GAME_NUMBER = int(sys.argv[1])
+    ENV_SHORT_NAME_PAYOFFS_LIST = []
+    for j in range(2, len(sys.argv)):
+        ENV_SHORT_NAME_PAYOFFS_LIST.append(sys.argv[j])
+    main(GAME_NUMBER, ENV_SHORT_NAME_PAYOFFS_LIST)
---
+++ att_graph_runner/plot_mean_payoffs.py
@@ -0,0 +1,153 @@
+import sys
+import math
+import numpy as np
+from plot_payoffs_auto import get_all_payoffs, get_round_count, \
+    get_cutoff_dev_payoffs, plot_payoffs
+
+def get_means(payoff_lists):
+    result = []
+    max_len = 0
+    for cur_list in payoff_lists:
+        if cur_list:
+            if cur_list[-1] is None:
+                max_len = max(max_len, len(cur_list) - 1)
+            else:
+                max_len = max(max_len, len(cur_list))
+    for i in range(max_len):
+        sum_i = sum([x[i] if len(x) > i and x[i] is not None \
+            else 0 for x in payoff_lists])
+        count_i = sum([1 if len(x) > i and x[i] is not None \
+            else 0 for x in payoff_lists])
+        if count_i == 0:
+            result.append(None)
+        else:
+            result.append(sum_i * 1.0 / count_i)
+    return result
+
+def get_standard_errors(payoff_lists):
+    result = []
+    max_len = 0
+    for cur_list in payoff_lists:
+        if cur_list:
+            if cur_list[-1] is None:
+                max_len = max(max_len, len(cur_list) - 1)
+            else:
+                max_len = max(max_len, len(cur_list))
+    for i in range(max_len):
+        cur_values = [x[i] for x in payoff_lists if len(x) > i and x[i] is not None]
+        if not cur_values:
+            result.append(0.0)
+        else:
+            cur_stdev = np.std(cur_values)
+            cur_count = len(cur_values)
+            cur_standard_error = cur_stdev * 1.0 / math.sqrt(cur_count)
+            result.append(cur_standard_error)
+    return result
+
+def get_gain(net_payoff, eq_payoff):
+    if net_payoff is None or eq_payoff is None:
+        return 0
+    return max(net_payoff - eq_payoff, 0)
+
+def get_all_mean_gains_with_standard_errors(game_number, env_short_name_payoffs_list, \
+    env_short_name_tsv_list):
+    all_att_gains = []
+    all_def_gains = []
+    for i in range(len(env_short_name_payoffs_list)):
+        env_short_name_payoffs = env_short_name_payoffs_list[i]
+        env_short_name_tsv = env_short_name_tsv_list[i]
+        round_count = get_round_count(env_short_name_tsv)
+        att_eq_payoffs, def_eq_payoffs, att_net_payoffs, def_net_payoffs = \
+            get_all_payoffs(round_count, env_short_name_tsv, \
+            env_short_name_payoffs, game_number)
+        cur_att_gains = [get_gain(att_net_payoffs[j], att_eq_payoffs[j]) for j in \
+            range(len(att_eq_payoffs))]
+        cur_def_gains = [get_gain(def_net_payoffs[j], def_eq_payoffs[j]) for j in \
+            range(len(def_eq_payoffs))]
+        all_att_gains.append(cur_att_gains)
+        all_def_gains.append(cur_def_gains)
+    mean_att_gain = get_means(all_att_gains)
+    mean_def_gain = get_means(all_def_gains)
+    sem_att_gain = get_standard_errors(all_att_gains)
+    sem_def_gain = get_standard_errors(all_def_gains)
+    return mean_att_gain, mean_def_gain, sem_att_gain, sem_def_gain
+
+def get_all_mean_payoffs_with_standard_errors(game_number, env_short_name_payoffs_list, \
+    env_short_name_tsv_list):
+    all_att_eq = []
+    all_def_eq = []
+    all_att_net = []
+    all_def_net = []
+    for i in range(len(env_short_name_payoffs_list)):
+        env_short_name_payoffs = env_short_name_payoffs_list[i]
+        env_short_name_tsv = env_short_name_tsv_list[i]
+        round_count = get_round_count(env_short_name_tsv)
+        att_eq_payoffs, def_eq_payoffs, att_net_payoffs, def_net_payoffs = \
+            get_all_payoffs(round_count, env_short_name_tsv, \
+            env_short_name_payoffs, game_number)
+        all_att_eq.append(att_eq_payoffs)
+        all_def_eq.append(def_eq_payoffs)
+        all_att_net.append(att_net_payoffs)
+        all_def_net.append(def_net_payoffs)
+    mean_att_eq = get_means(all_att_eq)
+    mean_def_eq = get_means(all_def_eq)
+    mean_att_net = get_means(all_att_net)
+    mean_def_net = get_means(all_def_net)
+    sem_att_eq = get_standard_errors(all_att_eq)
+    sem_def_eq = get_standard_errors(all_def_eq)
+    sem_att_net = get_standard_errors(all_att_net)
+    sem_def_net = get_standard_errors(all_def_net)
+    return mean_att_eq, mean_def_eq, mean_att_net, mean_def_net, \
+        sem_att_eq, sem_def_eq, sem_att_net, sem_def_net
+
+def get_all_mean_payoffs(game_number, env_short_name_payoffs_list, \
+    env_short_name_tsv_list):
+    all_att_eq = []
+    all_def_eq = []
+    all_att_net = []
+    all_def_net = []
+    for i in range(len(env_short_name_payoffs_list)):
+        env_short_name_payoffs = env_short_name_payoffs_list[i]
+        env_short_name_tsv = env_short_name_tsv_list[i]
+        round_count = get_round_count(env_short_name_tsv)
+        att_eq_payoffs, def_eq_payoffs, att_net_payoffs, def_net_payoffs = \
+            get_all_payoffs(round_count, env_short_name_tsv, \
+            env_short_name_payoffs, game_number)
+        all_att_eq.append(att_eq_payoffs)
+        all_def_eq.append(def_eq_payoffs)
+        all_att_net.append(att_net_payoffs)
+        all_def_net.append(def_net_payoffs)
+    mean_att_eq = get_means(all_att_eq)
+    mean_def_eq = get_means(all_def_eq)
+    mean_att_net = get_means(all_att_net)
+    mean_def_net = get_means(all_def_net)
+    return mean_att_eq, mean_def_eq, mean_att_net, mean_def_net
+
+def main(game_number, env_short_name_payoffs_list):
+    print("Using environments: " + str(env_short_name_payoffs_list))
+    env_short_name_tsv_list = [x + "_randNoAndB" for x in \
+        env_short_name_payoffs_list]
+    mean_att_eq, mean_def_eq, mean_att_net, mean_def_net = \
+        get_all_mean_payoffs(game_number, env_short_name_payoffs_list, \
+        env_short_name_tsv_list)
+    mean_att_net = get_cutoff_dev_payoffs(mean_att_eq, mean_att_net)
+    mean_def_net = get_cutoff_dev_payoffs(mean_def_eq, mean_def_net)
+    print(mean_att_eq)
+    print(mean_att_net)
+    print(mean_def_eq)
+    print(mean_def_net)
+    save_env_name = env_short_name_payoffs_list[0] + "_mean"
+    plot_payoffs(mean_def_eq, mean_def_net, mean_att_eq, mean_att_net, \
+        save_env_name)
+
+'''
+example: python3 plot_mean_payoffs.py 3014 d30d1 d30m1
+'''
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        raise ValueError("Need 2+ args: game_number, env_short_name_payoffs+")
+    GAME_NUMBER = int(sys.argv[1])
+    ENV_SHORT_NAME_PAYOFFS_LIST = []
+    for j in range(2, len(sys.argv)):
+        ENV_SHORT_NAME_PAYOFFS_LIST.append(sys.argv[j])
+    main(GAME_NUMBER, ENV_SHORT_NAME_PAYOFFS_LIST)
